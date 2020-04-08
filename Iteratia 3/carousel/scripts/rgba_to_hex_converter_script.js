06008e94bddfac05b89d2c9d53d99b624b651958
AndreiAvram99 <53441456+AndreiAvram99@users.noreply.github.com>
2020-04-08 16:10:49 +0300

    carousel

---
+++ Iteratia 3/carousel/scripts/rgba_to_hex_converter_script.js
@@ -0,0 +1,13 @@
+function trim (str) {
+    return str.replace(/^\s+|\s+$/gm,'');
+  }
+
+  function rgbaToHex (rgba) {
+    var parts = rgba.substring(rgba.indexOf("(")).split(","),
+        r = parseInt(trim(parts[0].substring(1)), 10),
+        g = parseInt(trim(parts[1]), 10),
+        b = parseInt(trim(parts[2]), 10),
+        a = parseFloat(trim(parts[3].substring(0, parts[3].length - 1))).toFixed(2);
+
+    return ('#' + r.toString(16) + g.toString(16) + b.toString(16));
+  }
